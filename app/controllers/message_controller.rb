c4341de062647353a11473eaa27a0c7e2b4e04f4
Will Sherman <vvj5@icloud.com>
2015-06-05 16:34:25 -0500

    Updated db and some route/controller fixes

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,44 +1,33 @@
 class MessageController < ApplicationController
 
-#Can I put .limit() on the end? Do I need to make a variablee first?
+#Can I put .limit() on the end? Do I need to make a variable first?
   def index
     render json: Message.order(created_at: :desc)
   end
 
-  def create_user
-    render json: Message.create(username: params[:username])
-  end
+  # def create_user
+  #   render json: Message.create(username: params[:username])
+  # end
 
-  def create_post
-    render json: Message.create(post: params[:post])
-  end
+  # def create_post
+  #   render json: Message.create(post: params[:post])
+  # end
 
-  def create_chatroom
-    render json: Message.create(chatroom: params[:chatroom])
+  # def create_chatroom
+  #   render json: Message.create(chatroom: params[:chatroom])
+  # end
+
+  def create
+    begin
+    message = Message.create(username: params.fetch(:username), post: params.fetch(:post), chatroom: params.fetch(:chatroom))
+      render json: message
+    rescue ActionController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
+    end
   end
 
   def show_all_users
-    render json: Message.all(params[:users])
+    render json: Message.all(params[:username])
   end
-
-  # def show
-  #   begin
-  #     render json: Todo.find(params[:id])
-  #   rescue ActiveRecord::RecordNotFound => error
-  #     render json: { error: error.message }, status: 422
-  #   end
-  # end
-
- #  def delete
- #    todo = Todo.find(params[:id])
- #    todo.destroy
- #    render json: { message: "Todo Detroyed" }
- #  end
-
- # def create
- #    message = Message.create(username: params.fetch(:username), entry: params.fetch(:entry))
- #    render json: message
- #  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,20 @@ Rails.application.routes.draw do
 #Display messages in descending chronological order
 get 'messages', to: 'message#index'
 
-#Create a new user
-post 'messages/create_user', to: 'message#create_user'
+post 'messages/create', to: 'message#create'
 
-#Show all users
-get 'messages/show_all_users', to: 'message#show_all_users'
+# #Create a new user
+# post 'messages/create_user', to: 'message#create_user'
 
-#Create a new post
-post 'messages/create_post', to: 'message#create_post'
+# #Show all users
+# get 'messages/show_all_users', to: 'message#show_all_users'
+
+# #Create a new post
+# post 'messages/create_post', to: 'message#create_post'
 
 root to: 'message#index'
 
 match '*not_found_route', to: 'application#skip_cors', via: [:get, :post, :put, :delete]
 
 end
-
-
-
-
-# get '/todos/', to: 'todos#index'
-
-# get '/todos/new', to: 'todos#new'
-
-# post '/todos/', to: 'todos#create'
-
-# delete '/todos/:id', to: 'todos#delete'
-
-# get '/todos/:id', to: 'todos#show'
-
 
