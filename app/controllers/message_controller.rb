c2fcf3d2ecb188e2b873b8d96814b55e862b44e4
Will Sherman <vvj5@icloud.com>
2015-06-06 15:44:25 -0500

    Sort Top Users (Fanatics) working locally

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -6,7 +6,7 @@ class MessageController < ApplicationController
 
   def create
     begin
-    message = Message.create(username: params.fetch(:username), post: params.fetch(:post), chatroom: params.fetch(:chatroom))
+      message = Message.create(username: params.fetch(:username), post: params.fetch(:post), chatroom: params.fetch(:chatroom))
       render json: message
     rescue ActionController::ParameterMissing => error
       render json: { error: error.message }, status: 422
@@ -14,46 +14,21 @@ class MessageController < ApplicationController
   end
 
   def recent_users
-      time_span = ((Time.now - 14400)..Time.now)
-      messages = Message.all
-      render json: messages.where(created_at: time_span).group(:username)
+    time_span = ((Time.now - 14400)..Time.now)
+    messages = Message.all
+    render json: messages.where(created_at: time_span).group(:username)
+  end
+
+  def fanatics
+    fanatics = Message.all.group(:username).count.sort_by { |username, count| count }.last(10).reverse
+    fanatics = Hash[fanatics]
+    render json: fanatics
+  end
+
+  def top_chatrooms
+    chatrooms = Message.all.group(:chatroom).count.sort_by { |chatroom, count| count }.last(10).reverse
+    chatrooms = Hash[chatrooms]
+    render json: chatrooms
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-###c
-  # def leaderboard
-  #   messages = Message.all
-  #   messages_array = messages.group(:username).count.sort_by { |name, count| count }
-  #   messages_array.reject! { |name, count| name == "" }
-  #   render json: Hash[messages_array.last(10).reverse]
-  # end
-
-###m
-#   def leaderboard
-#     leaders = Message.all.group(:username).count.sort_by { |username, count| count }.last(10).reverse
-#     leaders = Hash[leaders]
-#     render json: leaders
-#   end
-
-#   def recent_users
-#     recent_messages = Message.all.select { |message| message.created_at > (Time.now - 14400) }
-#     render json: recent_messages
-#   end
-
-#   def active_chatrooms
-#     chatrooms = Message.all.group(:chatroom).count.sort_by { |chatroom, count| count }.last(10).reverse
-#     chatrooms = Hash[chatrooms]
-#     render json: chatrooms
-#   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,47 +7,12 @@ post 'messages/create', to: 'message#create'
 
 get 'messages/recent_users', to: 'message#recent_users'
 
-get 'messages/leaderboard', to: 'message#leaderboard'
+get 'messages/fanatics', to: 'message#fanatics'
+
+get 'messages/top_chatrooms', to: 'message#top_chatrooms'
 
 root to: 'message#index'
 
 match '*not_found_route', to: 'application#skip_cors', via: [:get, :post, :put, :delete]
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###c
-#  get '/messages', to: 'messages#index'
-#  post 'messages/new', to: 'messages#create'
-#  get 'messages/leaderboard', to: 'messages#leaderboard'
-#  get 'messages/top_users', to: 'messages#top_users'
-#  match '*not_found_route', to: 'application#skip_cors', via: [:get, :post, :put, :delete]
-
-###m
-#  get "messages/leaderboard", to: "messages#leaderboard"
-#   get "messages/recent_users", to: "messages#recent_users"
-#   get "messages/active_chatrooms", to: "messages#active_chatrooms"
-#   get "messages", to: "messages#index"
-#   post "messages", to: "messages#create"
-
-###b
-#   get '/chat', to: 'chat_room#index' # getting all messages w/in last 4 hours
-#   get '/chat/history', to: 'chat_room#history'
-#   get '/chat/profile/:username', to: 'chat_room#profile'
-#   get '/leaderboard', to: 'chat_room#leaderboard'
-#   post '/new_user', to: 'chat_room#create_user'
-#   post '/chat', to: 'chat_room#post_msg'
-
